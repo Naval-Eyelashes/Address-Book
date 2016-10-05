a1f57162ca88a1bf1fac8dc447e3fdcfae981d9a
Kelly Wright <kelly.wright@kellyinnovation.com>
2016-10-05 14:14:13 -0400

    First pass with app.js.

--- app.js
+++ app.js
@@ -1,6 +1,38 @@
+'use strict';
+
+
+
+function mainController($timeout) {
+	const ctrl = this;
+	ctrl.people = [
+        {
+            firstName: "Jay",
+            lastName: "Willey",
+            phone: "859-111-1111",
+            email: "jay@jay.com",
+        },
+        {
+            firstName: "Kelly",
+            lastName: "Wright", 
+            phone: "859-222-2222",
+            email: "kelly@kelly.com",
+        }
+    ];
+
+    ctrl.newPerson = {
+    	firstName: '',
+    	lastName: '',
+    	phone: '',
+    	email: '',
+    }
+
+    function addPerson() {
+        ctrl.people.push(ctrl.newPerson);
+        ctrl.newPerson = {};
+    }
+
+}
+
+
 angular.module('app', [])
 	.controller('MainCtrl', mainController);
-
-function mainController($scope) {
-	Hello
-}
